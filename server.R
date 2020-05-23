6f733723d9f8b5e3a611488d3ba59d25b20407d8
nurainiaguse <aguse2@illinois.edu>
2020-05-23 18:36:51 -0500

    ui and interactivity updates

--- server.R
+++ server.R
@@ -11,22 +11,201 @@ library(ggplot2)
 library(rlist)
 library(phydose)
 library(plotly)
+library(shinydashboard)
 
 server <- function(input, output, session) {
     
-    # observe if file has been uploaded
+    # Reactive values ----------------------------------------------------------
+    values <- reactiveValues(
+        chosenTree = NULL,
+        chosenSample = NULL,
+        parse_data = NULL,
+        all_dff = NULL,
+        ret = NULL,
+        confSliderArgs = list("min"=0,"max"=0.99,"res"=0.01)
+    )
+    
+    phydoseData <- reactiveValues(
+        gamma_list = NULL,
+        cells = NULL,
+        all_dff = NULL,
+        per_tree_k = NULL,
+        parse_data = NULL
+    )
+    
+    # Observe if file has been uploaded ----------------------------------------
     observeEvent(input$file, {
         enable("run")
-        hide("displaymessage")
+        withProgress(message = 'Parsing file', value = 0, {
+            # 1. file is parsed to get trees, frequency matrix, and u matrix
+            incProgress(0.3, detail = "Checking file format")
+            values$parse_data <- ReadJoint(input$file[['datapath']])
+            
+            #parse_data <- ALLPatient2
+            # 2. using output from file parse, call the getdff to get dff
+            incProgress(0.3, detail = "Finding distinguishing features...")
+            values$all_dff <- generateDistFeat(values$parse_data$trees)
+        })
     })
     
-    # display help message
-    output$displaymessage <- renderText({ 
-        "Upload your file"
+    # Observe if specific tree is chosen to be displayed -----------------------
+    observeEvent(input$plotTree1,{
+        values$chosenTree <- as.numeric(input$treeNum)
     })
     
-    # run button is pressed
-    ret <- eventReactive(input$run, {
+    observeEvent(input$plotTree2,{
+        values$chosenTree <- as.numeric(input$treeNum2)
+    })
+    
+    observeEvent(input$clearChosenTree,{
+        values$chosenTree <- NULL
+    })
+    
+    observeEvent(event_data(event="plotly_click", source="mysource"),{
+        req(values$ret)
+        d <- event_data(event="plotly_click", source="mysource")
+        values$chosenTree <- as.numeric(d$key)
+        updateSelectInput(session, "treeNum", selected=as.numeric(d$key)
+        )
+    })
+    
+    observe({
+        req(values$ret)
+        labels <- list()
+        optionsid <- 1:length(values$ret$parse_data$trees)
+        for(i in 1:length(optionsid)){
+            lab <- paste("Tree", i)
+            labels[[lab]] <- i
+        }
+        updateSelectInput(session, "treeNum",
+                          label = paste0("Select tree [", length(labels)," trees]"),
+                          choices = labels
+        )
+        updateSelectInput(session, "treeNum2",
+                          label = paste0("Select tree [", length(labels)," trees]"),
+                          choices = labels
+        )
+    })
+    
+    observeEvent(input$treeNum, {
+        if (input$treeNum == ""){
+            return (NULL)
+        }
+        all_dff <- values$ret$all_dff
+        mydff <- all_dff[[as.numeric(input$treeNum)]]
+        dff_labels <- list()
+        for(i in 1:length(mydff)){
+            lab <- paste("DF", i)
+            dff_labels[[lab]] <- i
+        }
+        disp <- ""
+        
+        updateSelectInput(session, "dfNum",
+                          label = "Select distinguishing feature",
+                          choices = dff_labels
+        )
+        
+        featurettes <- mydff[[as.numeric(input$dfNum)]]
+        feat_labels <- list()
+        for(i in 1:length(featurettes)){
+            lab <- paste("Featurette", i)
+            feat_labels[[lab]] <- i
+        }
+        updateSelectInput(session, "featuretteNum",
+                          label = "Select featurette",
+                          choices = feat_labels
+        )
+    })
+    
+    observeEvent(input$treeNum2, {
+        if (input$treeNum2 == ""){
+            return (NULL)
+        }
+        all_dff <- values$ret$all_dff
+        mydff <- all_dff[[as.numeric(input$treeNum2)]]
+        dff_labels <- list()
+        for(i in 1:length(mydff)){
+            lab <- paste("DF", i)
+            dff_labels[[lab]] <- i
+        }
+        disp <- ""
+        
+        updateSelectInput(session, "dfNum2",
+                          label = "Select distinguishing feature",
+                          choices = dff_labels
+        )
+        
+        featurettes <- mydff[[as.numeric(input$dfNum2)]]
+        feat_labels <- list()
+        for(i in 1:length(featurettes)){
+            lab <- paste("Featurette", i)
+            feat_labels[[lab]] <- i
+        }
+        updateSelectInput(session, "featuretteNum2",
+                          label = "Select featurette",
+                          choices = feat_labels
+        )
+    })
+    
+    observeEvent(input$dfNum,{
+        if (input$dfNum == ""){
+            return (NULL)
+        }
+        mydff <- all_dff[[as.numeric(input$treeNum)]]
+        featurettes <- mydff[[as.numeric(input$dfNum)]]
+        feat_labels <- list()
+        for(i in 1:length(featurettes)){
+            lab <- paste("Featurette", i)
+            feat_labels[[lab]] <- i
+        }
+        updateSelectInput(session, "featuretteNum",
+                          label = "Select featurette",
+                          choices = feat_labels
+        )
+    })
+    
+    observeEvent(input$dfNum2,{
+        if (input$dfNum2 == ""){
+            return (NULL)
+        }
+        mydff <- all_dff[[as.numeric(input$treeNum2)]]
+        featurettes <- mydff[[as.numeric(input$dfNum2)]]
+        feat_labels <- list()
+        for(i in 1:length(featurettes)){
+            lab <- paste("Featurette", i)
+            feat_labels[[lab]] <- i
+        }
+        updateSelectInput(session, "featuretteNum2",
+                          label = "Select featurette",
+                          choices = feat_labels
+        )
+    })
+    
+    # ALL patient 1 is chosen
+    observeEvent(input$runALL, {
+        load(file="data/ALLphydose.rda")
+        values$ret <- list("my_glist"=ALLphydose$gamma_list,
+                    "my_clist"=ALLphydose$cells,
+                    "per_tree_k"=ALLphydose$per_tree_k,
+                    "parse_data"=ALLphydose$parse_data,
+                    "all_dff"=ALLphydose$all_dff
+        )
+        
+    })
+    
+    # AML patient 1 is chosen
+    observeEvent(input$runAML, {
+        load(file="data/AML38phydose.rda")
+        values$ret <- list("my_glist"=AML38phydose$my_glist,
+                           "my_clist"=AML38phydose$my_clist,
+                           "per_tree_k"=AML38phydose$per_tree_k,
+                           "parse_data"=AML38phydose$parse_data,
+                           "all_dff"=AML38phydose$all_dff
+        )
+    })
+    
+    # Run button is pressed
+    observeEvent(input$run, {
         ret <- NULL
         withProgress(message = 'Running PhyDOSE', value = 0, {
             # 1. file is parsed to get trees, frequency matrix, and u matrix
@@ -40,7 +219,10 @@ server <- function(input, output, session) {
             #all_dff <- ALLPatient2DFF
             # 3. after getting the dff, call phydose multiple times to get data for plot
             incProgress(0.2, detail = "Calculating values of k...")
-            gamma_list <- seq(0.0, 0.99, by=0.01)
+            gamma_list <- seq(input$gmin, input$gmax, by=input$gresolution)
+            if ( tail(gamma_list, 1) != input$gmax ) {
+                gamma_list <- c(gamma_list, input$gmax)
+            } 
             cells <- numeric(length(gamma_list))
             index <- 1
             u_list <- input$u
@@ -70,48 +252,6 @@ server <- function(input, output, session) {
             # 4. update UI
             incProgress(0.1, detail = "Updating UI...")
             # update the selection of trees
-            graphs <- ret$graphs
-            labels <- list()
-            optionsid <- 1:length(parse_data$trees)
-            print(paste("There are this many trees:", length(parse_data$trees)))
-            for(i in 1:length(optionsid)){
-                lab <- paste("Tree", i)
-                labels[[lab]] <- i
-            }
-            updateSelectInput(session, "treeNum",
-                              label = paste0("Select tree [", length(labels)," trees]"),
-                              choices = labels
-            )
-            
-            # update the selection of distinguishing features
-            mydff <- all_dff[[1]]
-            print("This is my dff")
-            print(mydff)
-            dff_labels <- list()
-            for(i in 1:length(mydff)){
-                lab <- paste("DF", i)
-                dff_labels[[lab]] <- i
-            }
-            disp <- ""
-            
-            updateSelectInput(session, "dfNum",
-                              label = "Select distinguishing feature",
-                              choices = dff_labels
-            )
-            
-            featurettes <- mydff[[1]]
-            print("These are the featurettes for my dff")
-            print(paste("I have this many featurettes", length(featurettes)))
-            print(featurettes)
-            feat_labels <- list()
-            for(i in 1:length(featurettes)){
-                lab <- paste("Featurette", i)
-                feat_labels[[lab]] <- i
-            }
-            updateSelectInput(session, "featuretteNum",
-                              label = "Select featurette",
-                              choices = feat_labels
-            )
             
             incProgress(0.2, detail = "Done!")
             ret <- list("my_glist"=gamma_list,
@@ -122,39 +262,35 @@ server <- function(input, output, session) {
             )
             incProgress(0.2, detail = "Done!!")
         })
-        
-        #res <- data.frame(k=cells, gamma = gamma_list)
-        # 4. get the dot representation of the selected tree (default to first tree)
-        # tree_diag <- plotTree(parse_data$trees[[1]])
-        # # 5. put everything in the return value
-        # ret <- list("my_glist"=gamma_list,
-        #             "my_clist"=cells,
-        #             "per_tree_k"=per_tree_k,
-        #             "parse_data"=parse_data
                     
         
-        return(ret)
+        values$ret <- ret
     })
     
-    # use this to hide the main panel (but there is a second delay so you can stillsee the panel for a sec)
-    output$timeToDisplay <- reactive({
-        return(!is.null(ret()))
-    })
-    outputOptions(output, 'timeToDisplay', suspendWhenHidden=FALSE)
-    
-    
-    # render the tree
+    # render tree 1
     output$graphV <- renderGrViz({ 
-        ret <- ret()
+        req(values$ret)
+        ret <- values$ret
         if (input$treeNum == ""){
             return (NULL)
         }
         tree <- ret$parse_data$trees[[as.numeric(input$treeNum)]]
-        featurette <- ret$all_dff[[as.numeric(input$treeNum)]][[as.numeric(input$dfNum)]][[as.numeric(input$featuretteNum)]]
+        featurette <- ret$all_dff[[as.numeric(input$treeNum)]][[as.numeric(input$dfNum)]][as.numeric(input$featuretteNum)]
         
         #print(ret$all_dff[[as.numeric(input$treeNum)]][[as.numeric(input$dfNum)]][[as.numeric(input$featuretteNum)]])
-        print("this is the featurette displayed")
-        print(featurette)
+        d_graph <- plotTree(tree, featurette=featurette)
+        render_graph(d_graph, layout="tree")
+    })
+    
+    # render tree 2
+    output$graphV2 <- renderGrViz({ 
+        req(values$ret)
+        ret <- values$ret
+        if (input$treeNum2 == ""){
+            return (NULL)
+        }
+        tree <- ret$parse_data$trees[[as.numeric(input$treeNum2)]]
+        featurette <- ret$all_dff[[as.numeric(input$treeNum2)]][[as.numeric(input$dfNum2)]][[as.numeric(input$featuretteNum2)]]
         d_graph <- plotTree(tree, featurette=featurette)
         render_graph(d_graph, layout="tree")
     })
@@ -162,19 +298,16 @@ server <- function(input, output, session) {
     output$kvscl <- renderPlotly({
         # generate data for plot
         # create the plot
-        ret <- ret()
+        req(values$ret)
+        ret <- values$ret
         gamma_list <- ret$my_glist
         cells_list <- ret$my_clist
         mydata <- data.frame(gamma = gamma_list, cells = cells_list)
 
-        if (input$plotThisTree){
-            mydata <- filter(ret$per_tree_k, tree_id == as.numeric(input$treeNum))
+        if (!is.null(values$chosenTree)){
+            mydata <- filter(ret$per_tree_k, tree_id == as.numeric(values$chosenTree))
         }
-        
         gamma_min <-filter(mydata, gamma >= input$gamma) %>% top_n(1, -gamma)
-        max_cells <- tail(mydata$cells, n=1)
-        max_cells <- ceiling(max_cells/10)*10
-        
         p <-ggplot(mydata, aes(x=cells, y=gamma)) + geom_step(size=1.5, color="darkblue") + ylab("confidence level") +
                 geom_hline(aes(yintercept = input$gamma), linetype = "dotdash") +
                 geom_vline(aes(xintercept=gamma_min$cells[1]), linetype = "dotdash") +
@@ -184,30 +317,73 @@ server <- function(input, output, session) {
     })
     
     output$box_jitter <- renderPlotly({
-        ret <- ret()
+        req(values$ret)
+        ret <- values$ret
         
-        max_cells <- tail(ret$per_tree_k$cells, n=1)
-        max_cells <- ceiling(max_cells/10)*10
+        max_cells <- max(ret$per_tree_k$cells)
+        #max_cells <- ceiling(max_cells/10)*10
         filtered_k <- filter(ret$per_tree_k, gamma >= input$gamma-0.001 & gamma <= input$gamma+0.001)
-        p <- ggplot(filtered_k, aes(x=sample, y=cells))+
+        p <- ggplot(filtered_k, aes(x=sample, y=cells,key=tree_id))+
             geom_boxplot() +
-            geom_jitter(position=position_jitter(width=0.5,height=0.01)) +
+            geom_jitter(position=position_jitter(width=0,height=0)) +
             scale_x_discrete(limits=c("1"), label=c("1"))+
             scale_y_continuous(limits=c(0,max_cells+1))+
             ylab("number of cells to sequence")+
             xlab("biopsy")
-            
-        ggplotly(p)
-        #%>% config(displayModeBar = FALSE)
+        if (!is.null(values$chosenTree)){
+            highlighted_k <- filter(filtered_k, tree_id == as.numeric(values$chosenTree))
+ 
+            p <- p + geom_point(highlighted_k, mapping=aes(), color="red", size=3)
+        }
+        ggplotly(p, source="mysource")
     })
     
     output$num_cells <- renderText({
-        ret <- ret()
+        req(values$ret)
+        ret <- values$ret
         gamma_list <- ret$my_glist
         cells_list <- ret$my_clist
-        mydata <- data.frame(g_list = gamma_list, c_list = cells_list)
+        mydata <- data.frame(gamma = gamma_list, cells = cells_list)
         
-        gamma_min <-filter(mydata, g_list >= input$gamma) %>% top_n(1, -g_list)
-        return(paste0(gamma_min$c_list[1], " cells required to sequence with confidence level = ", input$gamma))
+        if (!is.null(values$chosenTree)){
+            mydata <- filter(ret$per_tree_k, tree_id == as.numeric(values$chosenTree))
+        }
+        
+        gamma_min <-filter(mydata, gamma >= input$gamma) %>% top_n(1, -gamma)
+        return(paste0(gamma_min$cells[1], " cells required to sequence with confidence level = ", input$gamma))
+    })
+    
+    output$treeNumDetail <- renderText({
+        treeShown <- "-"
+        if (!is.null(values$chosenTree)){
+            treeShown <- values$chosenTree
+        }
+        return(paste0(
+            "Tree shown: ", treeShown
+        ))
+    })
+    
+    output$sampleNumDetail <- renderText({
+        sampleShown <- "-"
+        if (!is.null(values$chosenSample)){
+            sampleShown <- values$chosenSample
+        }
+        return(paste0(
+            "Biopsy shown: ", sampleShown
+        ))
+    })
+    
+    observe({
+        # Control the value, min, max, and step.
+        # Step size is 2 when input value is even; 1 when value is odd.
+        updateSliderInput(session, "gamma",
+                          min = input$gmin, max = input$gmax, step = min(input$gresolution,input$gmax-input$gmin))
+    })
+    
+    observe({
+        updateSliderInput(session, "gmin", max = input$gmax-0.01)
+    })
+    observe({
+        updateSliderInput(session, "gmax", min = input$gmin+0.01)
     })
 }
--- ui.R
+++ ui.R
@@ -8,80 +8,111 @@ library(shinyjs)
 library(DiagrammeR)
 library(shinyAce)
 library(ggplot2)
+library(rlist)
+library(phydose)
 library(plotly)
+library(shinydashboard)
 
-ui <- fluidPage(
-    useShinyjs(),
-    titlePanel("PhyDOSE-IT, v0.0.1"),
+ui <- dashboardPage(
+    dashboardHeader(title = "PhyDOSE-IT"),
     
-    tabsetPanel(
-        type = "tabs",
-        tabPanel(
-            "PhyDOSE-IT Interface", 
-            sidebarLayout(
-                sidebarPanel(
-                    fileInput("file", "Input file:", multiple = TRUE, 
-                              accept = c(".csv", ".tsv", ".txt")),
-                    sliderInput("quantile", "Quantile for k*:",0,1,1,step=0.01,ticks=FALSE),
-                    sliderInput("fn", "False negative rate:",0,0.5,0,step=0.01,ticks=FALSE),
-                    disabled(actionButton("run", "Run PhyDOSE")),
-                    textOutput("displaymessage"),
-                ),
-                #conditionalPanel(
-                #    condition="output.timeToDisplay == true",
-                    #h3("lwtxd",align="center")
-                mainPanel(
-                    fluidRow(
-                        column(12, align="center",
-                               h3(textOutput("num_cells")), 
-                            )
-                    ),
-                    fluidRow(
-                        column(
-                            8,
-                            plotlyOutput(outputId="kvscl"),
-                            align="center"
-                            ),
-                        column(
-                            4,
-                            plotlyOutput(outputId ="box_jitter"),
-                        )
-                    ),
-                    br(),
-                    fluidRow(
-                        column(12, align="center",
-                        sliderInput("gamma", 
-                                    "Confidence level",
-                                    0.01,0.99,0.95,
-                                    step=0.1,
-                                    ticks=FALSE),
-                        )
-                    ),
-                    hr(),
-                    h3("Visualize tree", align="center"),
-                    fluidRow(
-                        column(align="center",
-                            selectInput("treeNum", "Select Tree:",
-                                               c()),
-                            selectInput("dfNum", "Select Distinguishing Feature:",
-                                        c()),
-                            selectInput("featuretteNum", "Select Featurette:",
-                                        c()),
-                            checkboxInput("plotThisTree", "Plot this tree", FALSE),
-                            #h4(textOutput("which_dff")),
-                            #textOutput("display_dff"),
-                            width=5,
-                        ),
-                        column(align="center",grVizOutput('graphV'),width=7),
-                    ),
-                    
-                #),
-                )
+    dashboardSidebar(
+        fileInput("file", "Input file:", multiple = TRUE, 
+                  accept = c(".csv", ".tsv", ".txt")),
+        sliderInput("quantile", "Quantile for k*:",0,1,1,step=0.01,ticks=FALSE),
+        sliderInput("fn", "False negative rate:",0,0.5,0,step=0.01,ticks=FALSE),
+        actionButton("run", "Run PhyDOSE"),
+        sidebarMenu(
+            menuItem("Advanced toggles", 
+                     tabName="advanced",
+                     icon = icon("spinner"),
+                     sliderInput("gmin", "Gamma minimum:",0.01,0.99,0.01,step=0.01,ticks=FALSE),
+                     sliderInput("gmax", "Gamma maximum:",0.01,0.99,0.99,step=0.01,ticks=FALSE),
+                     sliderInput("gresolution", "Gamma resolution:",0.01,0.1,0.01,step=0.01,ticks=FALSE)
             )
+        ),
+        sidebarMenu(
+            menuItem("Example datasets", 
+                     tabName="egdatasets",
+                     icon = icon("spinner"),
+                     actionButton("runSim","Simulation dataset 1"),
+                     actionButton("runALL","ALL patient"),
+                     actionButton("runAML","AML patient")
+                     )
+        )
+    ),
+    
+    dashboardBody(
+        fluidRow(
+            column(12, align="center",
+                   h3(textOutput("num_cells")), 
+            )
+        ),
+        fluidRow(
+            box(
+                width=9,
+                plotlyOutput(outputId="kvscl"),
+            ),
+            box(
+                width=3,
+                plotlyOutput(outputId ="box_jitter"),
+            ),
+        ),
+        br(),
+
+        fluidRow(
+            box(
+                width=4,
+                h4("Details"),
+                textOutput("treeNumDetail"),
+                textOutput("sampleNumDetail"),
+                textOutput("quantileNumDetail")
+            ),
+                box(width=7, align="center",
+                       sliderInput("gamma", 
+                                   "Confidence level",
+                                   0.01,0.99,0.95,
+                                   step=0.01,
+                                   ticks=FALSE,
+                                   width='80%'),
                 ),
-        tabPanel("About", ),
-        tabPanel("FAQ", 
-                 )
+                actionButton("clearChosenTree", "Reset graphs"),
+        ),
+        fluidRow(
+            h3("Compare and visualize trees", align="center"),
+            box(
+                width = 6,
+                column(align="center",
+                       selectInput("treeNum", "Select Tree:",
+                                   c()),
+                       selectInput("dfNum", "Select Distinguishing Feature:",
+                                   c()),
+                       selectInput("featuretteNum", "Select Featurette:",
+                                   c()),
+                       actionButton("plotTree1", "Plot this tree"),
+                       width=5
+                ),
+                column(align="center",grVizOutput('graphV'),width=7),
+            ),
+            
+            
+            
+            box(
+                width = 6,
+                column(align="center",
+                       selectInput("treeNum2", "Select Tree:",
+                                   c()),
+                       selectInput("dfNum2", "Select Distinguishing Feature:",
+                                   c()),
+                       selectInput("featuretteNum2", "Select Featurette:",
+                                   c()),
+                       actionButton("plotTree2", "Plot this tree"),
+                       width=5
+                ),
+                column(align="center",grVizOutput('graphV2'),width=7),
+                ),
+        ),
+  
     ),
     
    
